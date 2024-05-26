01e342447e833564d5ca9f335daaae6982ab6a34
Mr.GeeKy <mr.geeky1992@gmail.com>
2024-05-26 08:36:50 +0430

    add placeholder

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementHomeComponent } from './element-home/element-home.component';
+import { PlaceHolderComponent } from './place-holder/place-holder.component';
 
 
 @NgModule({
   declarations: [
-    ElementHomeComponent
+    ElementHomeComponent,
+    PlaceHolderComponent
   ],
   imports: [
     CommonModule,
