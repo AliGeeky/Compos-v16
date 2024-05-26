feeed6ff49128bcf625935ebe9b2904b2556b4e9
Mr.GeeKy <mr.geeky1992@gmail.com>
2024-05-26 14:07:28 +0430

    initial ngcntent

--- src/app/shared/divider/divider.component.ts
+++ src/app/shared/divider/divider.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-divider',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 
 export class DividerComponent {
-
+  @Input ()title="";
 }
