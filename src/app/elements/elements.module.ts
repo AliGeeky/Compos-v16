754af3a7f71d1fefc4b6ce47cf23e3e69fac909a
Mr.GeeKy <mr.geeky1992@gmail.com>
2024-05-26 11:27:44 +0430

    Add shared Module

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -5,6 +5,7 @@ import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementHomeComponent } from './element-home/element-home.component';
 import { PlaceHolderComponent } from './place-holder/place-holder.component';
 import { RepeatTemplateDirective } from './repeat-template.directive';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { RepeatTemplateDirective } from './repeat-template.directive';
   ],
   imports: [
     CommonModule,
-    ElementsRoutingModule
+    ElementsRoutingModule,
+    SharedModule
   ],
 
 })
