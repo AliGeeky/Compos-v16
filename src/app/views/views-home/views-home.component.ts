21d8be8ad094f3b738c12853d2df1b705abed594
AliGeeKy <dotnetdev071@gmail.com>
2024-05-27 16:59:47 +0430

    Statictis Add and show

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+interface statictis {
+  value :number;
+  label:string;
+}
 
 @Component({
   selector: 'app-views-home',
@@ -6,5 +10,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./views-home.component.css']
 })
 export class ViewsHomeComponent {
+  data :statictis[] = [
+    { value: 28, label: '#Users' },
+    { value: 381, label: '#Like' },
+    { value: 63, label: '#Comment' },
+  ];
 
 }
