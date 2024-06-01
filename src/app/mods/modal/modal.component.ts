ee57b06f2601a9858f74e354cf7fd1723d300b2a
AliGeeKy <dotnetdev071@gmail.com>
2024-06-01 12:54:12 +0430

    ng-content (use ID)

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,23 +1,23 @@
-import { outputAst } from '@angular/compiler';
-import { Component, ElementRef, EventEmitter, OnInit, Output } from '@angular/core';
+import {Component,ElementRef,EventEmitter,Input,OnInit,Output} from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent implements OnInit{
-
-  constructor(private er:ElementRef){}
+export class ModalComponent implements OnInit {
+  constructor(private er: ElementRef) {}
   @Output() onClose = new EventEmitter();
-  ngOnInit():void {
+  @Input() isExistFooter = true;
+  @Input() defaultButton = true;
+  ngOnInit(): void {
     document.body.appendChild(this.er.nativeElement);
-  };
-  ngOnDestroy():void{
+  }
+  ngOnDestroy(): void {
     //document.body.removeChild(this.er.nativeElement);
     this.er.nativeElement.remove();
   }
-  OnCloseClick(){
+  OnCloseClick() {
     this.onClose.emit();
   }
 }
